b26313b73b242f6930c1ae0b1a3b549946f2338a
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-01-12 08:51:14 +0100

    bugg fix

--- IntegrationTests/Server/Server.cs
+++ IntegrationTests/Server/Server.cs
@@ -124,8 +124,8 @@ namespace Server
                     output.Write(buffer, 0, buffer.Length);
                     // You must close the output stream.
                     output.Close();
-                    listener.Stop();
-                }
+         
+            }
             }
         
 
